abf7766dd7665bf7373888bf8b0aee872c57c919
sharnack1 <32234951+sharnack1@users.noreply.github.com>
2017-09-24 13:25:26 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
+$access_token = 'I2lWl/T3XmN5orfzlcPMtKVevDPhe+HQzHpTsZj0FRY1iK4yVv5tZScn862+xYmSZ9ei1iMCD0xniyqQ8v6OZd849MrBEOkckchLmvbBSmXjnWMUYwKL7SFlJgwZEyfYDspyeAWo398e2tMzUpDJWAdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
