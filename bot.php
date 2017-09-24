04252fe4cb9f454713e8f7b2bfb9d1193afd64dd
sharnack1 <32234951+sharnack1@users.noreply.github.com>
2017-09-24 14:06:14 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -18,18 +18,18 @@ if (!is_null($events['events'])) {
 			
 			if($text =="สวัสดี")
 			{
-				$text ="สวัสดิ เหมือนกัน";
+				$text_reply ="สวัสดิ เหมือนกัน";
 			}
 			
 			if($text =="command->")
 			{
-				$text =" Bot ready for command";
+				$text_reply =" Bot ready for command";
 			}
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $text_reply
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
